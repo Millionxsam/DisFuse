12f6661c109106ae45638b199fc3721baf22b5e4
ddededodediamante <lukitadiamante5@gmail.com>
2024-10-04 16:00:08 -0300

    Add new local vars blocks, and new webhook blocks

--- src/Pages/Workspace.js
+++ src/Pages/Workspace.js
@@ -208,6 +208,8 @@ export default function Workspace() {
                   'err',
                   'files',
                   'filterItem',
+                  'localVar',
+                  'newWebhook',
                 ].forEach((word) => javascriptGenerator.addReservedWords(word));
 
                 if (project.owner.id !== user.id)
---
+++ src/blocks/localVars.js
@@ -0,0 +1,90 @@
+import * as Blockly from 'blockly';
+import { Order, javascriptGenerator } from 'blockly/javascript';
+
+Blockly.Blocks['localVars_set'] = {
+  init: function () {
+    var validator = function (newValue) {
+      if (/^[a-zA-Z0-9]+$/.test(newValue)) {
+        return newValue;
+      } else {
+        return null;
+      }
+    };
+
+    var varInput = new Blockly.FieldTextInput('variable');
+    varInput.setValidator(validator);
+
+    this.appendValueInput('value')
+      .setCheck(null)
+      .appendField('set')
+      .appendField(varInput, 'variable')
+      .appendField('to');
+    this.setPreviousStatement(true, 'default');
+    this.setNextStatement(true, 'default');
+    this.setColour('#d98e2b');
+  },
+};
+
+javascriptGenerator.forBlock['localVars_set'] = function (block, generator) {
+  var variable = block.getFieldValue('variable');
+  var value = generator.valueToCode(block, 'value', Order.ATOMIC) || 'null';
+
+  return `var ${variable} = ${value};\n`;
+};
+
+Blockly.Blocks['localVars_change'] = {
+  init: function () {
+    var validator = function (newValue) {
+      if (/^[a-zA-Z0-9]+$/.test(newValue)) {
+        return newValue;
+      } else {
+        return null;
+      }
+    };
+
+    var varInput = new Blockly.FieldTextInput('variable');
+    varInput.setValidator(validator);
+
+    this.appendValueInput('value')
+      .setCheck('Number')
+      .appendField('change')
+      .appendField(varInput, 'variable')
+      .appendField('by');
+    this.setPreviousStatement(true, 'default');
+    this.setNextStatement(true, 'default');
+    this.setColour('#d98e2b');
+  },
+};
+
+javascriptGenerator.forBlock['localVars_change'] = function (block, generator) {
+  var variable = block.getFieldValue('variable');
+  var value = generator.valueToCode(block, 'value', Order.ATOMIC) || 'null';
+
+  return `${variable} += ${value};\n`;
+};
+
+Blockly.Blocks['localVars_get'] = {
+  init: function () {
+    var validator = function (newValue) {
+      if (/^[a-zA-Z0-9]+$/.test(newValue)) {
+        return newValue;
+      } else {
+        return null;
+      }
+    };
+
+    var varInput = new Blockly.FieldTextInput('variable');
+    varInput.setValidator(validator);
+
+    this.appendDummyInput()
+      .appendField('value of variable')
+      .appendField(varInput, 'variable');
+    this.setOutput(true, null);
+    this.setColour('#d98e2b');
+  },
+};
+
+javascriptGenerator.forBlock['localVar_get'] = function (block, generator) {
+  var variable = block.getFieldValue('variable');
+  return [`${variable}`, Order.NONE];
+};
