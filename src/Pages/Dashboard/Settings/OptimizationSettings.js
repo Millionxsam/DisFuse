1b6f3c778e122dca7ed8a857410f25627e9d74a6
millionxsam <samdas3742@gmail.com>
2025-05-07 22:29:31 -0400

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/Pages/Dashboard/Settings/OptimizationSettings.js
+++ src/Pages/Dashboard/Settings/OptimizationSettings.js
@@ -1,66 +1,66 @@
-import { useState } from 'react';
-import Switch from '../../../components/Switch';
-import { useEffect } from 'react';
-import axios from 'axios';
-import LoadingAnim from '../../../components/LoadingAnim';
+import { useState } from "react";
+import Switch from "../../../components/Switch";
+import { useEffect } from "react";
+import axios from "axios";
+import LoadingAnim from "../../../components/LoadingAnim";
 
-let { discordUrl, apiUrl } = require('../../../config/config.json');
+let { discordUrl, apiUrl } = require("../../../config/config.js");
 
 export default function OptimizationSettings() {
-    const token = localStorage.getItem('disfuse-token');
-    const [user, setUser] = useState({});
-    const [loading, setLoading] = useState(true);
+  const token = localStorage.getItem("disfuse-token");
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
+    axios
+      .get(discordUrl + "/users/@me", {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then(({ data }) => {
         axios
-            .get(discordUrl + '/users/@me', {
-                headers: {
-                    Authorization: token,
-                },
-            })
-            .then(({ data }) => {
-                axios
-                    .get(apiUrl + `/users/${data.id}`, {
-                        headers: { Authorization: token },
-                    })
-                    .then(({ data: user }) => {
-                        setUser(user);
-                        setLoading(false);
-                    });
-            });
-    }, [token]);
-
-    function updateSetting(setting, value) {
-        if (typeof setting === 'object')
-            user.settings.optimization[setting[0]][setting[1]] = value;
-        else user.settings.optimization[setting] = value;
-
-        axios.put(apiUrl + `/users/${user.id}/settings`, user.settings, {
+          .get(apiUrl + `/users/${data.id}`, {
             headers: { Authorization: token },
-        });
-    }
+          })
+          .then(({ data: user }) => {
+            setUser(user);
+            setLoading(false);
+          });
+      });
+  }, [token]);
 
-    if (loading) return <LoadingAnim />;
+  function updateSetting(setting, value) {
+    if (typeof setting === "object")
+      user.settings.optimization[setting[0]][setting[1]] = value;
+    else user.settings.optimization[setting] = value;
 
-    return (
-        <>
-            <div className="settings">
-                <h1>Optimization</h1>
-                <p>
-                    Optimize the DisFuse website or workspace
-                </p>
-                <div className="option">
-                    <p>Fast block render:</p>
-                    <Switch
-                        defaultChecked={user.settings?.optimization.fastRenderMode ?? false}
-                        onChange={(e) => {
-                            updateSetting('fastRenderMode', e.currentTarget.checked);
-                        }}
-                        id="optimization-fastRenderMode"
-                    />
-                </div>
-                <h5>Makes the workspace run faster, but it might look more pixelated.</h5>
-            </div>
-        </>
-    );
+    axios.put(apiUrl + `/users/${user.id}/settings`, user.settings, {
+      headers: { Authorization: token },
+    });
+  }
+
+  if (loading) return <LoadingAnim />;
+
+  return (
+    <>
+      <div className="settings">
+        <h1>Optimization</h1>
+        <p>Optimize the DisFuse website or workspace</p>
+        <div className="option">
+          <p>Fast block render:</p>
+          <Switch
+            defaultChecked={user.settings?.optimization.fastRenderMode ?? false}
+            onChange={(e) => {
+              updateSetting("fastRenderMode", e.currentTarget.checked);
+            }}
+            id="optimization-fastRenderMode"
+          />
+        </div>
+        <h5>
+          Makes the workspace run faster, but it might look more pixelated.
+        </h5>
+      </div>
+    </>
+  );
 }
